de4fb45b2c2252863486782deb2080f679ce6542
ANAKIN\anakin <sentrino@naver.com>
2025-04-17 13:39:40 +0900

    커밋

--- consumers/src/main/java/com/example/kafka/ConsumerPartitionAssignSeek.java
+++ consumers/src/main/java/com/example/kafka/ConsumerPartitionAssignSeek.java
@@ -12,6 +12,30 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Properties;
 
+
+
+/* **
+ *  <kafka 토픽 생성 및 확인>
+ *   - 토픽 생성
+ *      kafka-topics --bootstrap-server localhost:9092 --delete --topic pizza-topic
+ *      kafka-topics --bootstrap-server localhost:9092 --create --topic pizza-topic --partitions 3
+ *   - 토픽 확인
+ *      kafka-topics --bootstrap-server localhost:9092 --describe --topic pizza-topic
+           : Topic: pizza-topic      TopicId: o6hyBGyITkazYGQskdvuUw PartitionCount: 3       ReplicationFactor: 1    Configs: segment.bytes=1073741824
+                Topic: pizza-topic      Partition: 0    Leader: 0       Replicas: 0     Isr: 0
+                Topic: pizza-topic      Partition: 1    Leader: 0       Replicas: 0     Isr: 0
+                Topic: pizza-topic      Partition: 2    Leader: 0       Replicas: 0     Isr: 0
+
+ *   - 메시지전송 테스트
+ *      kafka-console-producer --bootstrap-server localhost:9092  --topic pizza-topic
+ *   - 메시지수신 테스트
+ *      kafka-console-consumer --bootstrap-server localhost:9092 --topic pizza-topic
+ *      kafka-console-consumer --bootstrap-server localhost:9092 --topic pizza-topic --from-beginning
+ *   - offset 확인하기.
+ *      kafka-console-consumer --consumer.config /home/anakin/consumer_temp.config  --bootstrap-server localhost:9092 --topic __consumer_offsets  --formatter "kafka.coordinator.group.GroupMetadataManager\$OffsetsMessageFormatter" | grep simple-topic
+ * */
+
+
 public class ConsumerPartitionAssignSeek {
 
     public static final Logger logger = LoggerFactory.getLogger(ConsumerPartitionAssignSeek.class.getName());
@@ -24,7 +48,7 @@ public class ConsumerPartitionAssignSeek {
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
         props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        //props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_pizza_assign_seek_v001");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_pizza_assign_seek_v001");
         //props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "6000");
         props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
@@ -32,8 +56,10 @@ public class ConsumerPartitionAssignSeek {
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
         TopicPartition topicPartition = new TopicPartition(topicName, 0);
         //kafkaConsumer.subscribe(List.of(topicName));
+        
+        // 옵셋 지정하여 읽어 오는 옵션
         kafkaConsumer.assign(Arrays.asList(topicPartition));
-        kafkaConsumer.seek(topicPartition, 5L);
+        kafkaConsumer.seek(topicPartition, 30L);
 
         //main thread
         Thread mainThread = Thread.currentThread();
@@ -69,8 +95,6 @@ public class ConsumerPartitionAssignSeek {
                     logger.info("record key:{},  partition:{}, record offset:{} record value:{}",
                             record.key(), record.partition(), record.offset(), record.value());
                 }
-
-
             }
         }catch(WakeupException e) {
             logger.error("wakeup exception has been called");
@@ -80,7 +104,6 @@ public class ConsumerPartitionAssignSeek {
             logger.info("finally consumer is closing");
             kafkaConsumer.close();
         }
-
     }
 
     private static void pollCommitAsync(KafkaConsumer<String, String> kafkaConsumer) {
